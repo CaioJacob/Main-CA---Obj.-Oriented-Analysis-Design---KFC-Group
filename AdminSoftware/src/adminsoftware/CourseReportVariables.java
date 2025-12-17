dab199f54a080ddae6747140bf8035221602d391
Caio Jacob <147404364+CaioJacob@users.noreply.github.com>
2025-12-17 01:55:16 +0000

    fix(reports): standardize CourseReportVariables to use DatabaseConnection and remove credential parameters

--- AdminSoftware/src/adminsoftware/CourseReportVariables.java
+++ AdminSoftware/src/adminsoftware/CourseReportVariables.java
@@ -4,10 +4,7 @@
  */
 package adminsoftware;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -15,27 +12,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- *
- * @author User
+ * Query holder and fetcher for course/module reports.
  */
 public class CourseReportVariables {
-    
-    // Declared as final so that it can only be assigned to the right query
-    
-    public static final String sql = "SELECT m.ModuleName, m.Programme, COUNT(DISTINCT sm.StudentNumber) AS NumberOfStudents, l.LecturerName, m.Classroom FROM module m LEFT JOIN studentmodule sm ON m.ModuleID = sm.ModuleID LEFT JOIN lecturermodule lm ON m.ModuleID = lm.ModuleID INNER JOIN lecturer l ON lm.LecturerNumber = l.LecturerNumber GROUP BY m.ModuleName, m.Programme, l.LecturerName, m.Classroom HAVING l.LecturerName IS NOT NULL;";
 
-    public static List<CourseReportConstructor> fetchModuleInfo(String url, String user, String password) {
-    
+    public static final String sql =
+        "SELECT m.ModuleName AS ModuleName, m.Programme AS Programme, " +
+        "COUNT(DISTINCT sm.StudentNumber) AS NumberOfStudents, " +
+        "l.LecturerName AS LecturerName, m.Classroom AS Classroom " +
+        "FROM module m " +
+        "LEFT JOIN studentmodule sm ON m.ModuleID = sm.ModuleID " +
+        "LEFT JOIN lecturermodule lm ON m.ModuleID = lm.ModuleID " +
+        "LEFT JOIN lecturer l ON lm.LecturerNumber = l.LecturerNumber " +
+        "GROUP BY m.ModuleName, m.Programme, l.LecturerName, m.Classroom " +
+        "HAVING l.LecturerName IS NOT NULL;";
+
+    /**
+     * Fetch module/course information using DatabaseConnection.
+     * Note: signature no longer accepts DB credentials; DatabaseConnection reads env vars.
+     */
+    public static List<CourseReportConstructor> fetchModuleInfo() {
+
         List<CourseReportConstructor> modules = new ArrayList<>();
-     
+
         try (Connection conn = DatabaseConnection.getConnection();
              PreparedStatement pstmt = conn.prepareStatement(sql);
              ResultSet rs = pstmt.executeQuery()) {
 
             while (rs.next()) {
-                
-                // Assign values to variables
-                
+
                 String moduleName = rs.getString("ModuleName");
                 String programme = rs.getString("Programme");
                 String numberOfStudents = rs.getString("NumberOfStudents");
@@ -44,11 +49,12 @@ public class CourseReportVariables {
 
                 modules.add(new CourseReportConstructor(moduleName, programme, numberOfStudents, lecturerName, classroom));
             }
-            } catch (SQLException e) {
-                System.err.println("Error fetching course data: " + e.getMessage());
-            }  
 
-        return modules;
+        } catch (SQLException e) {
+            System.err.println("Error fetching course data: " + e.getMessage());
+            e.printStackTrace();
         }
 
+        return modules;
+    }
 }
